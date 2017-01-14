57df438943d1558510250c2690f127c04790c497
Matt Bell <mappum@gmail.com>
2017-01-14 14:31:42 -0800

    Allow index queries

--- src/client.js
+++ src/client.js
@@ -4,6 +4,9 @@ const old = require('old')
 const Tendermint = require('tendermint')
 const types = require('./types.js')
 
+const QUERY_BY_INDEX = 0
+const QUERY_BY_KEY = 1
+
 class Client {
   constructor (node) {
     if (typeof node === 'string' || typeof node === 'undefined') {
@@ -13,14 +16,26 @@ class Client {
     }
   }
 
-  query (key, cb) {
-    let query = types.query.encode({ type: 1, key })
+  query (type, key, cb) {
+    let query = types.query.encode({ type, key })
     this.tendermint.abciQuery({ query }, (err, res) => {
       if (err) return cb(err)
       let resJson = Buffer.from(res[1].result.Data, 'hex').toString()
       return JSON.parse(resJson)
     })
   }
+
+  get (key, cb) {
+    return this.query(QUERY_BY_KEY, key, cb)
+  }
+
+  getIndex (index, cb) {
+    let indexBuf = Buffer.alloc(4)
+    indexBuf.writeUInt32BE(index, 0)
+    return this.query(QUERY_BY_INDEX, indexBuf, cb)
+  }
+
+  // TODO: set, remove
 }
 
 module.exports = old(Client)
